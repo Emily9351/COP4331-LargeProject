31519cfd151a03f64b184a73280ec58892d52285
Emily Densmore <emily.densmore12@gmail.com>
2026-03-29 23:53:43 -0400

    fix to work on server

--- web-frontend/src/pages/Registration.tsx
+++ web-frontend/src/pages/Registration.tsx
@@ -19,7 +19,7 @@ export function Registration() {
     try {
         const name = `${firstName} ${lastName}`;
 
-        const response = await fetch("http://localhost:5000/api/register", {
+        const response = await fetch("/api/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ name, email, password }),
