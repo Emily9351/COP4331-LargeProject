520befbb38ffb81ccd8dad71d7ea7d7a06ca2e50
Emily Densmore <emily.densmore12@gmail.com>
2026-04-14 19:46:00 -0400

    push

--- mobile-frontend/lib/pages/login_page.dart
+++ mobile-frontend/lib/pages/login_page.dart
@@ -23,7 +23,7 @@ class _LoginPageState extends State<LoginPage> {
 
     try {
       final response = await http.post(
-        Uri.parse('http://167.99.63.238:5000/api/login'),
+        Uri.parse('http://emilydensmore.com:5000/api/login'),
         headers: {'Content-Type': 'application/json'},
         body: jsonEncode({'email': email, 'password': password}),
       );
